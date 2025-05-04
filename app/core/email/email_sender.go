3840093eb16ac3ac337bc83da18ea7161890bd3d
TvTTiT <thanhtvfs0830@gmail.com>
2025-05-05 00:15:46 +0200

    wip

--- app/core/email/email_sender.go
+++ app/core/email/email_sender.go
@@ -27,7 +27,7 @@ func (s *SMTPEmailSender) Send(email EmailDomain.EmailMessage) error {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	to := []string{email.To}
 	msg := []byte(fmt.Sprintf("Subject: %s\r\nFrom: %s\r\nTo: %s\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n%s",
-		utils.SanitizeInput(email.Subject), utils.SanitizeInput(email.From), utils.SanitizeInput(email.To), utils.SanitizeInput(email.Body)))
+		utils.SanitizeInput(email.Subject), utils.SanitizeInput(email.From), utils.SanitizeInput(email.To), email.Body))
 
 	addr := fmt.Sprintf("%s:%s", s.smtpHost, s.smtpPort)
 	err := smtp.SendMail(addr, nil, email.From, to, msg)
